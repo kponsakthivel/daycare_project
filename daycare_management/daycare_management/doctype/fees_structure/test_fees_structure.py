1572f35956f0c9fadb059717f30fdc88266d83e4
kponsakthivel <kponsakthivel@gmail.com>
2024-04-28 21:00:38 +0530

    Update Frappe app files

---
+++ daycare_management/daycare_management/doctype/fees_structure/test_fees_structure.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, ponsakthivel and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestFeesStructure(FrappeTestCase):
+	pass
---
+++ daycare_management/daycare_management/doctype/organization/organization.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, ponsakthivel and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Organization", {
+// 	refresh(frm) {
+
+// 	},
+// });
